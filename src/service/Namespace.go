eef8ece8391ccf9db0a85df2c3c2f4f7052ca33a
bryant <bryant.ba520@gmail.com>
2023-01-29 11:53:50 +0800

    first commit

---
+++ src/service/Namespace.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	. "src/lib"
+)
+
+func ListNamespace(g *gin.Context) {
+	ns, err :=
+}
